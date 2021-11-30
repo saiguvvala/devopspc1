3a7c5bed1f37c95187d2dd92bfa4804a6747782f
saiguvvala <gv.saikumar321@gmail.com>
2021-11-30 19:07:57 +0530

    cfn chnages

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -22,7 +22,7 @@ SNSBUCKETNAME=$(jq -r ".s3SnsBucketName" < ./Jenkins/cfn.json)
 # do a aws ocnfigure in single line reading the secrets from the github secrets
 aws configure set aws_access_key_id ${ACCESS_KEY}
 aws configure set aws_secret_access_key ${SECRET_KEY}
-aws configure set default.region eu-west-2
+aws configure set default.region ap-south-1
 
 ##
 # Assuming the roles required to deploy on the target account.
