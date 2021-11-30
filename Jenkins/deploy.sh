ee0a434a6d0ba235c42bb5bd5ab39c09ed023708
saiguvvala <gv.saikumar321@gmail.com>
2021-11-30 19:24:41 +0530

    path changes

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -38,32 +38,39 @@ export AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}"
 ##
 # Package infrastructure
 ##
-ls -la
-
- 
-cd "${GITHUB_WORKSPACE}"/infrastructure
-
-ls -la
+cd "${WORKSPACE}"
+aws cloudformation package \
+    --template-file infrastructure/aws-stacks/vpcstack.yml \
+    --s3-bucket "${ARTIFACT_NAME}" \
+    --output-template-file infrastructure/aws-stacks/vpcstack_release.yaml
 
 aws cloudformation package \
-    --template-file aws-stacks/s3updates-stack.yml \
+    --template-file infrastructure/aws-stacks/loadbalancer.yml \
     --s3-bucket "${ARTIFACT_NAME}" \
-    --output-template-file aws-stacks/s3updates-stack_release.yaml
+    --output-template-file infrastructure/aws-stacks/loadbalancer_release.yaml
 
-cd "${GITHUB_WORKSPACE}"
+aws cloudformation package \
+    --template-file infrastructure/aws-stacks/apistack.yml \
+    --s3-bucket "${ARTIFACT_NAME}" \
+    --output-template-file infrastructure/aws-stacks/apistack_release.yaml
 
+aws cloudformation package \
+    --template-file infrastructure/aws-stacks/masterstack.yml \
+    --s3-bucket "${ARTIFACT_NAME}" \
+    --output-template-file infrastructure/aws-stacks/masterstack_release.yaml
 
 ##
 # Deploy infrastructure stack.
 ##
+cd "${WORKSPACE}"
 aws cloudformation deploy \
-    --template-file "${GITHUB_WORKSPACE}"/infrastructure/aws-stacks/s3updates-stack_release.yaml \
-    --stack-name "${TARGET_ENVIRONMENT}"-"${PREFIX}"-event-stack\
+    --template-file infrastructure/aws-stacks/masterstack_release.yaml \
+    --stack-name "${TARGET_ENVIRONMENT}"-"${PREFIX}"-master \
 	--s3-bucket "${ARTIFACT_NAME}" \
 	--capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM \
-	--no-fail-on-empty-changeset \
+    --no-fail-on-empty-changeset \
 	--parameter-overrides \
-	Environment="${TARGET_ENVIRONMENT}" \
     Prefix="${PREFIX}" \
-    BucketName="${SNSBUCKETNAME}" \
-    
+    Environment="${TARGET_ENVIRONMENT}" \
+    KeyName="${KEYNAME}" \
+    ArtifactBucket="${ARTIFACT_NAME}"
